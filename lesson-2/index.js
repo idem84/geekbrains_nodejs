134973c948b28f22d7dbea2124901932050c66a0
Dmitriy <idem84@me.com>
2021-12-15 21:35:26 +0300

    hw2 done

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,103 +1,48 @@
-const colors = require("colors");
-const getPrimes = (start, max) => {
-  // var sieve = [],
-  //   i,
-  //   j,
-  //   primes = [];
-  // for (i = start; i <= max; ++i) {
-  //   if (!sieve[i]) {
-  //     primes.push(i);
-  //     for (j = i << 1; j <= max; j += i) {
-  //       sieve[j] = true;
-  //     }
-  //   }
-  // }
-  // return primes;
-  primes = [];
+"use strict";
+const ps = require("prompt-sync");
+const moment = require("moment");
+const prompt = ps();
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+emitter.setMaxListeners(1);
 
-  for (let i = start; i <= max; i++) {
-    let flag = 0;
+let dates = new Date(
+  prompt("Select Date: year, month, day, time (2021,12,15,22:01):")
+);
 
-    // looping through 2 to user input number
-    for (let j = 2; j < i; j++) {
-      if (i % j == 0) {
-        flag = 1;
+const init = async () => {
+  let future = dates;
+  let now = new Date();
+  let difference = future - now;
+  let duration = moment.duration(difference, "milliseconds");
+  let timer;
+  let payload = 1;
+  timer = {
+    years: duration._data.years,
+    months: duration._data.months,
+    days: duration._data.days,
+    hours: duration._data.hours,
+    minutes: duration._data.minutes,
+    seconds: duration._data.seconds,
+  };
+  if (timer.seconds > 0) {
+    payload = `${timer.years} years, ${timer.months} months, ${timer.days} days, ${timer.hours} hours, ${timer.minutes} minutes, ${timer.seconds} seconds`;
+  } else {
+    console.log("Timer end");
 
-        break;
-      }
-    }
-
-    // if number greater than 1 and not divisible by other numbers
-    if (i > 1 && flag == 0) {
-      primes.push(i);
-    }
+    return false;
   }
 
-  return primes;
+  emitter.emit("timer", payload);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await init();
 };
 
-const printColorNumber = (number, index) => {
-  switch (index) {
-    case 0:
-      console.log(colors.green(number));
-
-      break;
-    case 1:
-      console.log(colors.yellow(number));
-
-      break;
-    case 2:
-      console.log(colors.red(number));
-
-      break;
-
-    default:
-      console.log(colors.blue(number));
-
-      break;
-  }
+const timer = (payload) => {
+  console.log("Till end: " + payload);
 };
 
-let args = process.argv;
-let range = args.slice(-2);
-let start = parseInt(range[0]);
-let end = parseInt(range[1]);
-
-if (isNaN(start) || isNaN(end)) {
-  console.log("Error, please enter two integers");
-
-  return false;
-}
-
-if (start < 2) {
-  console.log("Error, start number must be more then 1");
-
-  return false;
-}
-
-const numbers = getPrimes(start, end);
-
-if (numbers.length < 1) {
-  console.log("Error, there are no primes in the specified range");
-
-  return false;
-}
-
-let iterator = 1;
-
-numbers.forEach((element) => {
-  if (iterator === 1) {
-    console.log(colors.green(element));
-    iterator++;
-    if (iterator > 3) iterator = 1;
-  } else if (iterator === 2) {
-    console.log(colors.yellow(element));
-    iterator++;
-    if (iterator > 3) iterator = 1;
-  } else if (iterator === 3) {
-    console.log(colors.red(element));
-    iterator++;
-    if (iterator > 3) iterator = 1;
-  }
-  //printColorNumber(element, i);
-});
+console.log("Timer start!");
+emitter.on("timer", timer);
+init();
