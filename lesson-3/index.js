aa6dcc15f8822eedbac2b635d7bab5bb7a16891d
Dmitriy <idem84@me.com>
2021-12-21 14:56:49 +0300

    hw3 done

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -1,48 +1,32 @@
 "use strict";
-const ps = require("prompt-sync");
-const moment = require("moment");
-const prompt = ps();
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
-emitter.setMaxListeners(1);
+const fs = require("fs");
+let readline = require("readline");
 
-let dates = new Date(
-  prompt("Select Date: year, month, day, time (2021,12,15,22:01):")
-);
+const ACCESS_LOG = "./access.log";
+const IP_LIST_1 = "./192.168.174.254.log";
+const IP_LIST_2 = "./192.168.174.162.log";
 
-const init = async () => {
-  let future = dates;
-  let now = new Date();
-  let difference = future - now;
-  let duration = moment.duration(difference, "milliseconds");
-  let timer;
-  let payload = 1;
-  timer = {
-    years: duration._data.years,
-    months: duration._data.months,
-    days: duration._data.days,
-    hours: duration._data.hours,
-    minutes: duration._data.minutes,
-    seconds: duration._data.seconds,
-  };
-  if (timer.seconds > 0) {
-    payload = `${timer.years} years, ${timer.months} months, ${timer.days} days, ${timer.hours} hours, ${timer.minutes} minutes, ${timer.seconds} seconds`;
-  } else {
-    console.log("Timer end");
+const rd = readline.createInterface({
+  input: fs.createReadStream(ACCESS_LOG),
+  terminal: false,
+});
 
-    return false;
+const writeStream = fs.createWriteStream(IP_LIST_1, {
+  flags: "a",
+  encoding: "utf-8",
+});
+
+const writeStream2 = fs.createWriteStream(IP_LIST_2, {
+  flags: "a",
+  encoding: "utf-8",
+});
+
+rd.on("line", (line) => {
+  let result = /192.168.174.254/g.test(line);
+  let result2 = /192.168.174.162/g.test(line);
+  if (result) {
+    writeStream.write(line + "\n");
+  } else if (result2) {
+    writeStream2.write(line + "\n");
   }
-
-  emitter.emit("timer", payload);
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  await init();
-};
-
-const timer = (payload) => {
-  console.log("Till end: " + payload);
-};
-
-console.log("Timer start!");
-emitter.on("timer", timer);
-init();
+});
