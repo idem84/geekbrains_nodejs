fe436db41935139f5aa22e6d0b56f6b1a35d70a2
Dmitriy <idem84@me.com>
2022-01-10 15:45:24 +0300

    hw5 is done

--- lesson-5/index.js
+++ lesson-5/index.js
@@ -1,32 +1,40 @@
-"use strict";
+const http = require("http");
+const path = require("path");
 const fs = require("fs");
-let readline = require("readline");
 
-const ACCESS_LOG = "./access.log";
-const IP_LIST_1 = "./192.168.174.254.log";
-const IP_LIST_2 = "./192.168.174.162.log";
+const run = async () => {
+  const isFile = (path) => fs.lstatSync(path).isFile();
+  http
+    .createServer((req, res) => {
+      console.log(req.url);
+      const fullPath = path.join(process.cwd(), req.url);
+      if (!fs.existsSync(fullPath))
+        return res.end("File or directory not found");
+      if (isFile(fullPath)) {
+        return fs.createReadStream(fullPath).pipe(res);
+      }
+      let linksList = "";
+      const urlParams = req.url.match(/[\d\w\.]+/gi);
+      if (urlParams) {
+        urlParams.pop();
+        const prevUrl = urlParams.join("/");
+        linksList = urlParams.length
+          ? `<li><a href="/${prevUrl}">..</a></li>`
+          : '<li><a href="/">..</a></li>';
+      }
+      fs.readdirSync(fullPath).forEach((fileName) => {
+        const filePath = path.join(req.url, fileName);
+        linksList += `<li><a href="${filePath}">${fileName}</a></li>`;
+      });
+      const HTML = fs
+        .readFileSync(path.join(__dirname, "index.html"), "utf-8")
+        .replace("##links", linksList);
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      return res.end(HTML);
+    })
+    .listen(5555);
+};
 
-const rd = readline.createInterface({
-  input: fs.createReadStream(ACCESS_LOG),
-  terminal: false,
-});
-
-const writeStream = fs.createWriteStream(IP_LIST_1, {
-  flags: "a",
-  encoding: "utf-8",
-});
-
-const writeStream2 = fs.createWriteStream(IP_LIST_2, {
-  flags: "a",
-  encoding: "utf-8",
-});
-
-rd.on("line", (line) => {
-  let result = /192.168.174.254/g.test(line);
-  let result2 = /192.168.174.162/g.test(line);
-  if (result) {
-    writeStream.write(line + "\n");
-  } else if (result2) {
-    writeStream2.write(line + "\n");
-  }
-});
+run();
